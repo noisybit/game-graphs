44acbb9596d6f0778bb679ba3cde55a10bb5a2dc
Brian Hassinger <brian.a.hassinger@gmail.com>
2013-07-01 11:52:37 -0500

    add main graph x-axis reviews

--- js/graph.js
+++ js/graph.js
@@ -1,7 +1,7 @@
 $(function() {
   var margin = {top: 20, right: 40, bottom: 60, left: 25};
   var width = window.innerWidth - margin.left - margin.right;
-  var height = (window.innerHeight / 1.8)- margin.top - margin.bottom;
+  var height = 240 - margin.top - margin.bottom;
   var bars = 1400;
   var barWidth = Math.floor(width / bars) - 1
   var color = d3.scale.category10();
@@ -14,36 +14,26 @@ $(function() {
       .attr('height', height + margin.top + margin.bottom)
     .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-      
-  var yBar = d3.scale.linear()
+
+  var y = d3.scale.linear()
       .range([height, 0]);
 
-  var yBarAxis = d3.svg.axis()
-      .scale(yBar)
+  var yAxis = d3.svg.axis()
+      .scale(y)
       .orient('left')
-      .tickFormat(d3.format())
-
-  var yLine = d3.scale.linear()
-      .range([height, 0]);
-
-  var yLineAxis = d3.svg.axis()
-      .scale(yLine)
-      .orient('right')
       .tickFormat(d3.format())
 
   // Create the x scale
   var x = d3.scale.ordinal()
       .rangeBands([0, width], 0.1);
 
+  var xRatingScale = d3.scale.ordinal()
+      .rangeBands([0, width])
+
   var xAxis = d3.svg.axis()
-      .scale(x)
+      .scale(xRatingScale)
       .orient('bottom')
       .tickPadding(0.7)
-
-  // Setup the line graph
-  var line = d3.svg.line()
-      .x(function(d, i) { return x(i) })
-      .y(function(d) { return yLine(d.reviewCount) });
 
   // Grab the data
   d3.json('/data/rated.json', function(err, data) {
@@ -64,16 +54,31 @@ $(function() {
         return reviews;
     })
 
-
+    var xRatingScaleDomain = data.filter(function(d, i, arr) {
+      var blockSize = Math.floor(arr.length / 8);
+      return i % blockSize  === Math.floor((blockSize / 2))
+    }).map(function(d){ return d.reviewCount; })
 
     // Scale the domains
-    yBar.domain([0, 10]);
-    yLine.domain([0, d3.max(data, function(d) { return d.reviewCount; })]);
+    y.domain([0, 10]);
+    x.domain(data.map(function(d, i){ return i; }));
+    xRatingScale.domain(xRatingScaleDomain)
 
-    // Draw the yBarAxis
+    // Draw the xAxis
     svg.append('g')
-        .attr('class', 'yAxis yBarAxis')
-        .call(yBarAxis)
+        .attr('class', 'xAxis')
+        .attr('transform', 'translate(0, '+height+')')
+        .call(xAxis)
+      .append('text')
+        .attr('y', 15)
+        .attr('x', width/2 - margin.left)
+        .attr('dy', '.71em')
+        .text('Reviews');
+
+    // Draw the yAxis
+    svg.append('g')
+        .attr('class', 'yAxis')
+        .call(yAxis)
       .append('text')
         .attr('y', -12)
         .attr('x', 6)
@@ -81,61 +86,22 @@ $(function() {
         .style('text-anchor', 'start')
         .text('Rating');
 
-    svg.append('g')
-        .attr('class', 'xAxis')
-        .attr('transform', 'translate(0,'+(height)+')')
+    // Draw the bar graph
+    var bargraph = svg.selectAll('.bar')
+          .data(data)
 
+    bargraph.enter().append('rect')
+        .attr('class', 'bar')
+        .attr('width', x.rangeBand())
+        .on('mouseover', showTooltip)
+        .on('mousemove', moveTooltip)
+        .on('mouseout', hideTooltip)
 
-    /*
-    // Draw the yLineAxis
-    svg.append('g')
-        .attr('class', 'yAxis yLineAxis')
-        .attr('transform', 'translate('+(window.innerWidth - margin.right - 40)+',0)')
-        .call(yLineAxis)
-      .append('text')
-        .attr('y', -12)
-        .attr('x', -6)
-        .attr('dy', '.71em')
-        .style('text-anchor', 'end')
-        .text('Reviews');
-    */
-
-    render();
-    function render() {
-      var visible = data.slice(index, index+bars);
-      x.domain(visible.map(function(d, i){ return d.title; }));
-
-      // Draw the bar graph
-      var bargraph = svg.selectAll('.bar')
-            .data(visible)
-
-      bargraph.enter().append('rect')
-          .attr('class', 'bar')
-          .attr('width', x.rangeBand())
-          .on('mouseover', showTooltip)
-          .on('mousemove', moveTooltip)
-          .on('mouseout', hideTooltip)
-
-      bargraph
-        .attr('x', function(d, i) { return x(d.title); })
-        .attr('y', function(d) { return yBar(d.rating); })
-        .attr('height', function(d) { return height - yBar(d.rating); })
-        .attr('fill', function(d) { return color(d.genre); })
-
-        /*
-      // Draw the line graph
-      var lines =  svg.selectAll('.line')
-          .data(visible)
-          .attr('d', line(visible))
-
-      lines.enter().append('path').attr('class', 'line')
-      lines.attr('d', line(visible));
-      lines.exit().remove();
-
-      svg.select('yLineAxis')
-        .call(yLineAxis);
-        */
-    }
+    bargraph
+      .attr('x', function(d, i) { return x(d.title); })
+      .attr('y', function(d) { return y(d.rating); })
+      .attr('height', function(d) { return height - y(d.rating); })
+      .attr('fill', function(d) { return color(d.genre); })
 
     // Draw the legend
     var legend = d3.select('#legend')
@@ -158,14 +124,6 @@ $(function() {
           .attr('dy', '.71em')
           .style('text-anchor', 'start')
           .text(function(d){ return d; });
-
-    // Offset data when mouse is scrolled
-    $('#graph').bind('mousewheel', function(e, delta) {
-      var scrollBy = -delta * scrollSpeed
-      var newIndex = index + scrollBy;
-      index = (newIndex >= 0 && newIndex < data.length - bars) ? newIndex: index;
-      render();
-    })
 
     var $tooltip = $('#gameInfo');
     function showTooltip(d) {
