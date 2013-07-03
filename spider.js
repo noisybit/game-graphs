29b1b2a109d7833a67f23c3fa6358b8685e3dc83
Brian Hassinger <brian.a.hassinger@gmail.com>
2013-07-03 15:48:52 -0500

    update spider

--- spider.js
+++ spider.js
@@ -1,34 +1,61 @@
+var fs = require('fs');
 var spider = require('spider');
-var redis = require('redis');
-var db = redis.createClient();
+var records = [];
+var system = process.argv[2] || 'snes';
+var categoryAll = new RegExp('/'+system+"/list-999\\?page=\\d+");
+var gameOverview = new RegExp('/'+system+"/\\d+[-\\w]+$");
 
 spider()
-.route('www.gamefaqs.com', /\/snes\/list-999\?page=\d+/, function(window, $) {
-  $('a').spider();
-})
-.route('www.gamefaqs.com', /\/snes\/\d+[-\w]+$/, function(window, $) {
-  var game = { title: $('.page-title').find('a').text() };
-
-  var $rating = $('.score').eq(1);
-  game.rating = parseFloat($rating.text(), 10);
-  if(game.rating === NaN)
-    game.rating = null;
-
-  //Number of reviews
-  game.reviewCount = (game.rating === null)
-  ? null
-  : $rating.next('a').text().split(' ')[1]
-
-  game.boxart = $('img.boxshot').attr('src');
-  game.release = $('.pod_gameinfo li').eq(3).find('a').text().slice(0, -2);
-  game.genre = $('.crumb a').eq(1).text();
-
-  db.incr('db.recordCount', function(err, key) {
-    db.hmset('games:id:'+key, game, function(err) {
-      console.log('%s - %s - %d - %d', game.title, game.release, game.rating, game.reviewCount);
-      window.close();
-    });
+  .route('www.gamefaqs.com', categoryAll, function(window, $) {
+    $('a').spider();
+    window.close();
   })
+  .route('www.gamefaqs.com', gameOverview, function(window, $) {
+
+    var game = {}
+    var $rating = $('.score').eq(1);
+    game.rating = parseFloat($rating.text(), 10);
+
+    // No reviews :( - DO NOT WANT!
+    if(isNaN(game.rating))
+      return window.close();
+
+    game.reviews = $rating.next('a').text().split(' ')[1];
+    game.title = $('.page-title').find('a').text();
+    game.boxart = $('img.boxshot').attr('src');
+    game.release = $('.pod_gameinfo li').eq(3).find('a').text().slice(0, -2);
+    game.genre = $('.crumb a').eq(1).text();
+    game.subgenre = $('.crumb a').eq(2).text();
+    game.href = 'http://www.gamefaqs.com' + $('.page-title').find('a').attr('href');
+
+    records.push(game);
+    log(game);
+    window.close();
+  })
+  .get('http://www.gamefaqs.com/'+system+'/list-999\?page=0')
+  .log('error');
+
+process.on('exit', function() {
+  console.log('Process terminating...')
+  var data = {
+    system: system,
+    timestamp: Date.now(),
+    records: records
+  };
+  var str = JSON.stringify(data);
+  var filename = __dirname+'/data/'+system+'-'+data.timestamp+'.json';
+
+  if(records.length) {
+    fs.writeFileSync(filename, str, 'utf8');
+    console.log('%d records saved!', records.length);
+  }
 })
-.get('http://www.gamefaqs.com/snes/list-999\?page=0')
-.log('error')
+
+process.on('SIGINT', function(){ process.exit() });
+
+function log(game) {
+  var msg = game.rating + '/10';
+  msg += ' \"'+ (game.title.length > 50 ? game.title.slice(0, 47) + '...' : game.title) + '\"';
+  msg += ' ['+game.genre+']['+game.subgenre+']';
+  console.log(msg);
+}
